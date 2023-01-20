b39bbcea4cecae2672cc3ae0d0515581245f6545
timDeHof <this.is.tim.dehof@gmail.com>
2023-01-20 11:30:10 -0500

    cleaned up the unused files

---
+++ src/routes/movieslist.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Movieslist = () => {
+  return <div>Movieslist</div>
+}
---
+++ src/routes/watchlist.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Watchlistatchlist = () => {
+  return <div>Watchlist</div>
+}
