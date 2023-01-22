e8f035d2320805a44faeb348d38b9fbc00acb5a4
timDeHof <this.is.tim.dehof@gmail.com>
2023-01-21 21:11:06 -0500

    added a layout to watchlist

--- src/routes/watchlist.js
+++ src/routes/watchlist.js
@@ -1,12 +1,26 @@
-import { Movie } from "@mui/icons-material"
-import { IconButton } from "@mui/material"
-import React from "react"
+import { AddCircle } from "@mui/icons-material"
+import { Box, Button, Typography } from "@mui/material"
 import { Link } from "react-router-dom"
+import React from "react"
+import { MovieCard } from "../components"
 
 export const Watchlist = () => {
   return (
-    <IconButton color='inherit' component={Link} to='/'>
-      <Movie />
-    </IconButton>
+    <Box flex={4} p={2}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "0 auto",
+          height: "auto",
+          width: { xs: "100%", sm: "35%" },
+        }}>
+        <Typography>Your watchlist is looking a little empty...</Typography>
+        <Button variant='text' component={Link} to='/' startIcon={<AddCircle />} size='small'>
+          Let’s add some movies!
+        </Button>
+      </Box>
+    </Box>
   )
 }
